6250fe957a3fcb19c06c9e340bad4921717df3b6
cinttybr <cinthya.barrezueta.reyes@gmail.com>
2017-10-25 18:53:10 -0500

    actualizamos usuarioDTO

--- src/beans/UsuarioDTO.java
+++ src/beans/UsuarioDTO.java
@@ -1,13 +1,5 @@
 package beans;
 
-/* `cod_usu` varchar(11) NOT NULL,
-  `nom_usu` varchar(50) NOT NULL,
-  `ape_usu` varchar(50) NOT NULL,
-  `fono_usu` varchar(20) DEFAULT NULL, -- si se piensa dar formato(otras ciudades¿?) deberia ser varchar¿?
-  `correo` varchar(50) DEFAULT NULL,
-  `login_usu` varchar(15) NOT NULL,
-  `pass_usu` varchar(20) NOT NULL,
- `cod_estado` int(5) NOT NULL references tb_estado,*/
 
 public class UsuarioDTO 
 {

97b0bf624ea47b3eaf432aeab75aa80993a9640f
DanteCRMSN <32529689+DanteCRMSN@users.noreply.github.com>
2017-11-02 17:21:58 -0500

    Update MySqlInstitucionDAO.java

--- src/dao/MySqlInstitucionDAO.java
+++ src/dao/MySqlInstitucionDAO.java
@@ -6,12 +6,175 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import beans.ComentariosDTO;
 import beans.InstitucionDTO;
+import beans.InstitucionNombresDTO;
+import beans.ReporteComentCalificacionDTO;
 import interfaces.InstitucionDAO;
 import utils.MySqlDBConexion;
 
 public class MySqlInstitucionDAO implements InstitucionDAO
 {
+	@Override
+	public int registrarComentariosInstitucion(ComentariosDTO obj) {
+		
+		int estado=-1;
+		Connection cn=null;
+		PreparedStatement pstm=null;
+		try {
+			cn= new MySqlDBConexion().getConexion();
+			String sql="update tb_institucion set comentarios=?,calificacion=? where cod_ins=?";
+			pstm=cn.prepareStatement(sql);
+			pstm.setString(1, obj.getComentarios());
+			pstm.setInt(2, obj.getCalificacion());
+			pstm.setInt(3, obj.getCodigo());
+			estado=pstm.executeUpdate();
+		} catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				if(pstm!=null)pstm.close();
+				if(cn!=null)cn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return estado;
+	}
+	
+	@Override
+	public List<ReporteComentCalificacionDTO> listarComentariosCalificacion() {
+		List<ReporteComentCalificacionDTO> data = new ArrayList<ReporteComentCalificacionDTO>();
+		ReporteComentCalificacionDTO obj = null;
+		Connection cn = null;
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+				try {
+					cn = new MySqlDBConexion().getConexion();
+					String sql= "select i.nom_ins, i.comentarios, c.calificacion from tb_institucion i inner join tb_calificacion c on i.cod_calificacion=c.cod_calificacion";
+					pstm = cn.prepareStatement(sql);
+					rs = pstm.executeQuery();
+					while (rs.next()){
+						obj = new ReporteComentCalificacionDTO();
+						obj.setNombre(rs.getString(1));
+						obj.setComentarios(rs.getString(2));
+						obj.setCalificacion(rs.getString(3));
+						data.add(obj);
+					}
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+				return data;
+	}
+	
+	@Override
+	public List<InstitucionNombresDTO> listarInstitucionNombres() {
+		List<InstitucionNombresDTO> data = new ArrayList<InstitucionNombresDTO>();
+		InstitucionNombresDTO obj = null;
+		Connection cn = null;
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+				try {
+					cn = new MySqlDBConexion().getConexion();
+					String sql= "select cod_ins,nom_ins from tb_institucion";
+					pstm = cn.prepareStatement(sql);
+					rs = pstm.executeQuery();
+					while (rs.next()){
+						obj = new InstitucionNombresDTO();
+						obj.setCodigo(rs.getInt(1));
+						obj.setNombre(rs.getString(2));
+						data.add(obj);
+					}
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+				return data;
+	}
+	
+	@Override
+	public ComentariosDTO buscarInstitucionCOMENTARIOS(int cod) {
+		
+		ComentariosDTO obj = null;
+		Connection cn = null;
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		
+		try{
+			cn = new MySqlDBConexion().getConexion();
+			String sql = "SELECT i.comentarios,c.calificacion FROM tb_institucion "+
+			" i inner join tb_calificacion c on i.cod_calificacion=c.cod_calificacion WHERE cod_ins=?";
+			pstm = cn.prepareStatement(sql);
+			pstm.setInt(1, cod);
+			rs = pstm.executeQuery();
+			if(rs.next())
+			{
+				obj = new ComentariosDTO();
+				obj.setComentarios(rs.getString(1));
+				obj.setCalificacion(rs.getInt(2));	
+			}
+			
+		}catch(SQLException ex)
+		{
+			ex.printStackTrace();
+		}finally
+		{
+			try{
+				if(rs!=null)rs.close();
+				if(pstm!=null)pstm.close();
+				if(cn!=null)cn.close();
+				
+			}catch(Exception e2)
+			{
+				e2.printStackTrace();
+			}
+		}
+		
+		return obj;
+	}
+
+
+/**//*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	@Override
 	public InstitucionDTO buscarInstitucion(int cod) {
 		
